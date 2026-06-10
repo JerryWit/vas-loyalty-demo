6f2792b86b4d9a8291def3bb1cbdddf90352f31b
JerryWit <biuro@rantheri.com>
2026-06-10 20:03:38 +0200

    demo1

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import AdminConfig from './pages/AdminConfig.jsx'
+import LenderDemo from './pages/LenderDemo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/admin" element={<AdminConfig />} />
+        <Route path="/lender-demo" element={<LenderDemo />} />
         <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
